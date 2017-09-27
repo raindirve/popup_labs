e67720ccd49e06fffd33d06ce1d18d8662ab84f2
Michal Horemuz <michalh@kth.se>
2017-09-27 13:18:03 +0200

    polymult

--- lab1/polymult.h
+++ lab1/polymult.h
@@ -46,6 +46,45 @@ void ifft(std::valarray<std::complex<double> > & a) {
 }
 
 
+typedef long long llong;
+
+std::vector<double> polymult(const std::vector<double> & a_, const std::vector<double> & b_ ) {
+	int na = a_.size();
+	int nb = b_.size();
+	
+	int max_power = na + nb; // maximum possible power from the result of the mult
+	
+	int s = 1;
+	while(s < max_power) { // smallest power of 2 larger than max power
+		s <<= 1;
+	}
+	
+	std::valarray<std::complex<double>> a(s);		// need complex numbers for the fft
+	std::valarray<std::complex<double>> b(s);
+	
+	for(int i = 0; i<na; ++i) {
+		a[i] = std::complex<double>(a_[i],0);
+	}
+	
+	for(int i = 0; i<nb; ++i) {
+		b[i] = std::complex<double>(b_[i],0);
+	}
+	
+	fft(a); // fft both polynomials
+	fft(b);
+	
+	for(int j = 0; j<s; ++j) {  // convolution becomes pointwise multiplication
+		a[j] *= b[j];
+	}
+	
+	ifft(a); // inverse fft
+	
+	std::vector<double> ans(s); // output vector
+	for(int j = 0; j<s; ++j) {
+		ans[j] = a[j].real();
+	}
+	return ans;
+}
 
 
 
